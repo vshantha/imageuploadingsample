5381711971d8f7786d84c2fa8a0551cdab5a429b
vshantha <vshantha@umail.iu.edu>
2016-05-26 06:50:53 -0400

    Add files via upload
    Added PHP server side scripts

---
+++ dbConnect.php
@@ -0,0 +1,11 @@
+<?php
+ define('HOST','');
+ define('USER','');
+ define('PASS','');
+ define('DB','');
+ 
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+ 
+ if($con == true){
+    //printf("Successful");
+}
